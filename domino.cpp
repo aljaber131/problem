2bad9d2f585407f296963e2b87169e948cbb6e03
aljaber131 <60335077+aljaber131@users.noreply.github.com>
2021-04-26 09:56:10 -0700

    Add files via upload

---
+++ domino.cpp
@@ -0,0 +1,12 @@
+/*
+https://codeforces.com/problemset/problem/50/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    int M,N;
+    cin>>M>>N;
+    int ans=(M*N)/2;
+    cout<<ans;
+}
